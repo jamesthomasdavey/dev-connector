a4acb0ec9a1d77bfc8134db5bd70871138b8cf88
James Davey <james.t.davey@gmail.com>
2018-10-22 13:04:03 -0700

    add post route for post (sounds redundant, I know)

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,34 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// import models
+const Post = require('./../../models/Post');
+
+// import validation
+const validatePostInput = require('./../../validation/post');
 
 // @route     get api/posts/test
 // @desc      tests posts route
 // @access    public
-router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
+router.get('/test', (req, res) => res.json({ msg: 'Posts works' }));
+
+// @route     post api/posts
+// @desc      create post
+// @access    private
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validatePostInput(req.body);
+
+  // check valiation
+  if (!isValid) return res.status(400).json(errors);
+  const newPost = new Post({
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    user: req.user._id
+  });
+  newPost.save().then(post => res.json(post));
+});
 
 module.exports = router;
